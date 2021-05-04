a4a4085dae005fab4b88f5a73202edb3ffe0475a
skillfool <Repyanski@gmail.com>
2021-05-04 21:36:05 +0300

    Oauth2 Google and RememberMe

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -92,13 +92,6 @@ Vue.component('messages-list', {
         '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
             ':editMethod="editMethod" :messages="messages" />' +
     '</div>',
-  created: function() {
-    messageApi.get().then(result =>
-        result.json().then(data =>
-            data.forEach(message => this.messages.push(message))
-        )
-    )
-  },
   methods: {
     editMethod: function(message) {
         this.message = message;
@@ -107,9 +100,20 @@ Vue.component('messages-list', {
 });
 
 var app = new Vue({
-  el: '#app',
-  template: '<messages-list :messages="messages" />',
-  data: {
-    messages: []
-  }
+    el: '#app',
+    template:
+        '<div>' +
+        '<div v-if="!profile">Authorize with <a href="/login">Google</a></div>' +
+        '<div v-else>' +
+        '<div>{{profile.name}}&nbsp;<a href="/logout">Log Out</a></div>' +
+        '<messages-list :messages="messages" />' +
+        '</div>' +
+        '</div>',
+    data: {
+        messages: frontendData.messages,
+        profile: frontendData.profile
+    },
+    created: function () {
+
+    },
 });
