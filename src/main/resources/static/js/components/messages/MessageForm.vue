15741ae85bbe78cff10c66c33d7ded2ad7e16f77
skillfool <Repyanski@gmail.com>
2021-05-05 19:20:24 +0300

    Add WebSocket (using SockJS + Stomp)

--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -3,19 +3,10 @@
     <input type="text" placeholder="Write something" v-model="text"/>
     <input type="button" value="Save" @click="save"/>
   </div>
-
 </template>
 
 <script>
-function getIndex(list, id) {
-  for (var i = 0; i < list.length; i++) {
-    if (list[i].id === id) {
-      return i
-    }
-  }
-
-  return -1
-}
+import {sendMessage} from 'util/ws'
 
 export default {
   props: ['messages', 'messageAttr'],
@@ -33,28 +24,31 @@ export default {
   },
   methods: {
     save() {
-      const message = {text: this.text}
+      sendMessage({id: this.id, text: this.text})
+      this.text = ''
+      this.id = ''
 
-      if (this.id) {
-        this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
-            result.json().then(data => {
-              const index = getIndex(this.messages, data.id)
-              this.messages.splice(index, 1, data)
-              this.text = ''
-              this.id = ''
-            })
-        )
-      } else {
-        this.$resource('/message{/id}').save({}, message).then(result =>
-            result.json().then(data => {
-              this.messages.push(data)
-              this.text = ''
-            })
-        )
-      }
+      /*const message = { text: this.text }
+
+if (this.id) {
+this.$resource('/message{/id}').update({id: this.id}, message).then(result =>
+  result.json().then(data => {
+    const index = getIndex(this.messages, data.id)
+    this.messages.splice(index, 1, data)
+    this.text = ''
+    this.id = ''
+  })
+)
+} else {
+this.$resource('/message{/id}').save({}, message).then(result =>
+  result.json().then(data => {
+    this.messages.push(data)
+    this.text = ''
+  })
+)
+      }*/
     }
   }
-
 }
 </script>
 
