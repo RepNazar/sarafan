9b0b045b5d4dfff71a29c0494e8d5bdbb5a6cee5
skillfool <Repyanski@gmail.com>
2021-05-13 00:50:58 +0300

    Vuex store

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue'
+import store from 'store/store'
 import {connect} from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
 
-if(frontendData.profile) {
+if (frontendData.profile) {
     connect()
 }
 
@@ -13,8 +15,9 @@ Vue.use(Vuetify)
 
 new Vue({
     el: '#app',
+    store,
     vuetify: new Vuetify({
-        theme: {default:true}
+        theme: {default: true}
     }),
     render: a => a(App)
 })
